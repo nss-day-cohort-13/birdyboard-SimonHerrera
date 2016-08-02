ab5df08a0a6deafa3faef00bf927909679e60b50
Simon Herrera <simonherrera.tech@gmail.com>
2016-08-01 23:53:30 -0500

    Test Bypass

--- birdyboard_tests.py
+++ birdyboard_tests.py
@@ -1,42 +1,55 @@
 import unittest
-from birdyboard.py import Birdyboard
+from birdyboard import Birdyboard
 # from unittest.mock import Mock, patch, DEFAULT - look into (de/serilize)
 
 class TestBirdyboard (unittest.TestCase):
 
   @classmethod
-  def setUpClass(self):
+  def setUp(self):
     self.birdyboard = Birdyboard()
 
   def test_user_created(self):
-    # tests to go here
+    assert 1 == 1
+    # actual tests to go here
 
 
   def test_user_selected(self):
-    # tests to go here
+    assert 2 == 2
+    # actual tests to go here
 
 
   def test_view_chirps(self):
-    # tests to go here
+    assert 3 == 3
+    # actual tests to go here
 
 
   def test_write_public_chirp(self):
-    # tests to go here
+    assert 4 == 4
+    # actual tests to go here
 
 
   def test_write_private_chirp(self):
-    # tests to go here
+    assert 5 == 5
+    # actual tests to go here
 
 
 
 
   #Test all options except quit and veriy a non valid entry fails
   def test_menu_option_choices(self):
+    assert 10 == 10
     # Decide if I need to test choices
 
 
 
 if __name__ == '__main__':
     unittest.main()
-    # print('Running')
+
+# to run unittest...
 # python birdyboard_tests.py -v
+
+# to run coverage ...
+# coverage run birdboard.py
+# coverage report
+# coverage report -a (to see all lines not covered)
+# covrerage html (to create folder w report)
